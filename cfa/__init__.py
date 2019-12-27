2cdae8dd7a5a18c5cad4d2422f51dbfa0474a8f8
Renan <renanhs80@gmail.com>
2019-12-27 14:10:46 -0300

    Alter host and port.

--- cfa/__init__.py
+++ cfa/__init__.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -12,6 +13,8 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+port = int(os.environ.get("PORT", 5000))
+manager.add_command('runserver', Server(host='0.0.0.0', port=port))
 
 lm = LoginManager(app)
 lm.login_view = 'auth.login'
--- manage.py
+++ manage.py
@@ -1,7 +1,5 @@
-import os
 from cfa import manager
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    manager.run(host="0.0.0.0", port=port)
+    manager.run()
