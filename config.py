735c346a84e2d942ba950b0cb0086d621af5b596
Renan <renanhs80@gmail.com>
2019-12-27 14:50:40 -0300

    Alteração para teste.

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'cfa.db')
-#SECRET_KEY = '4NBn148Y4VkzFnN6o8Gk'
+SECRET_KEY = '4NBn148Y4VkzFnN6o8Gk'
 #SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-app.config['SECRET_KEY'] = SECRET_KEY
+#app.config['SECRET_KEY'] = SECRET_KEY
