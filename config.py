07a7ec40edc7a0294b256c4cb051d4707e94782e
Renan <renanhs80@gmail.com>
2019-12-27 14:37:33 -0300

    Migrate for postgre.

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-import os.path
+import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'cfa.db')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'cfa.db')
+#SECRET_KEY = '4NBn148Y4VkzFnN6o8Gk'
+#SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-SECRET_KEY = '4NBn148Y4VkzFnN6o8Gk'
