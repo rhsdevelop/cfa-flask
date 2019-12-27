016902dcb2eab2b2762826902f237ac25472c56a
Renan <renanhs80@gmail.com>
2019-12-27 14:47:47 -0300

    Secret key

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from cfa import app
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
@@ -9,3 +11,4 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+app.config['SECRET_KEY'] = SECRET_KEY
